b17e2b524b5ec3dfec47ffc1355a9629ad6d8ff4
cheruto <joyglory213@gmail.com>
2026-01-22 18:09:47 +0300

    update posts

--- users/views.py
+++ users/views.py
@@ -11,8 +11,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get( 'username')
-            messages.success(request, f'Your account is now created! You are now able to log in')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
